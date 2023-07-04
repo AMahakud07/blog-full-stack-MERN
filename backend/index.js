f25fe5fc0e4eda8fc98b61803b6e67171786c469
AMahakud07 <abhishekmahakud007@gmail.com>
2023-07-04 21:39:31 +0530

    login issue

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const cors=require('cors')
 const User = require('./models/User')
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
 
 const app = express();
+const jwt = require('jsonwebtoken')
+
+const salt = bcrypt.genSaltSync(10);
+const secret = 'sfdsdfrgsd34f';
 
 app.use(cors());
 
@@ -14,9 +19,35 @@ mongoose.connect('mongodb+srv://blog:rxqA6IZSCK2bqmT7@cluster0.1hlyzic.mongodb.n
 
 app.post('/register',async(req,res)=>{
     const {username,password} = req.body;
-    const userDoc =await User.create({username,password})
-    res.json(userDoc);
+    try{
+        const userDoc =await User.create({
+            username,
+            password: bcrypt.hashSync(password,salt),
+        })
+        res.json(userDoc);
+    } catch (e){
+        res.status(400).json(e);
+    }
 });
+
+app.post('/login',async (req,res)=>{
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username})
+    res.json(userDoc);
+    const passOk =bcrypt.compareSync(password, userDoc.password);
+    // res.json(passOk);
+    if(passOk){
+        //logged in
+        jwt.sign({username,id:userDoc._id},secret,{},(err,token)=>{
+            if(err) throw err;
+            res.json(token);
+        })
+        // res.json()
+    }else{
+        //not logged in
+        res.status(400).json('wrong credentials');
+    }
+})
 
 app.listen(4000);
 
--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,10 +1,33 @@
+import { useState } from "react"
+
 export default function LoginPage(){
+    const [username,setUsername]= useState('');
+    const [password,setPassword]= useState('');
+    function login(e){
+        e.preventDefault();
+
+        fetch('http://localhost:4000/login',{
+            method: 'POST',
+            body: JSON.stringify({username,password}),
+            headers:{'Content-Type':'application/json'}
+        })
+    }
     return (
         <>
         <form className="login">
             <h1>Login</h1>
-            <input type="text" placeholder="usrename"/>
-            <input type="password" placeholder="password"/>
+            <input 
+            type="text" 
+            placeholder="usrename"
+            value={username}
+            onChange={(e)=> setUsername(e.target.value)}
+            />
+            <input 
+            type="password" 
+            placeholder="password"
+            value={password}
+            onChange={(e)=> setPassword(e.target.value)}
+            />
             <button>Login</button>
         </form>
         </>
