deaceaf0a2c7b81613e10841b3379bdb2a409312
AMahakud07 <abhishekmahakud007@gmail.com>
2023-07-10 21:54:41 +0530

    create post

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const cors=require('cors')
+const cors = require('cors')
 const User = require('./models/User')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
 
 const app = express();
 const jwt = require('jsonwebtoken')
@@ -11,60 +13,64 @@ const jwt = require('jsonwebtoken')
 const salt = bcrypt.genSaltSync(10);
 const secret = '545fgeretr43rgtery545ery45yery4566erefwg';
 
-app.use(cors({credentials:true,origin:'http://localhost:3000'}));
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json()) // to read data from req body
 app.use(cookieParser());
 
 mongoose.connect('mongodb+srv://blog:rxqA6IZSCK2bqmT7@cluster0.1hlyzic.mongodb.net/?retryWrites=true&w=majority');
 
 
-app.post('/register',async(req,res)=>{
-    const {username,password} = req.body;
-    try{
-        const userDoc =await User.create({
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const userDoc = await User.create({
             username,
-            password: bcrypt.hashSync(password,salt),
+            password: bcrypt.hashSync(password, salt),
         })
         res.json(userDoc);
-    } catch (e){
+    } catch (e) {
         res.status(400).json(e);
     }
 });
 
-app.post('/login',async (req,res)=>{
-    const {username,password} = req.body;
-    const userDoc = await User.findOne({username})
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const userDoc = await User.findOne({ username })
     //res.json(userDoc);
-    const passOk =bcrypt.compareSync(password, userDoc.password);
+    const passOk = bcrypt.compareSync(password, userDoc.password);
     // res.json(passOk);
-    if(passOk){
+    if (passOk) {
         //logged in
-        jwt.sign({username,id:userDoc._id},secret,{},(err,token)=>{
-            if(err) throw err;
+        jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+            if (err) throw err;
             // res.json(token);
-            res.cookie('token',token).json({
+            res.cookie('token', token).json({
                 id: userDoc._id,
                 username,
             })
         })
         // res.json()
-    }else{
+    } else {
         //not logged in
         res.status(400).json('wrong credentials');
     }
 })
 
-app.get('/profile',(req,res)=>{
-    const {token} = req.cookies;
-    jwt.verify(token, secret, {},(err,info)=>{
+app.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, (err, info) => {
         if (err) throw err;
         res.json(info);
     })
     res.json(req.cookies)
 })
 
-app.post('/logout',(req,res)=>{
-    res.cookie('token','').json('ok');
+app.post('/logout', (req, res) => {
+    res.cookie('token', '').json('ok');
+})
+
+app.post('/post', uploadMiddleware.single('file'), (req, res) => {
+    res.json({ files: req.file })
 })
 
 app.listen(4000);
