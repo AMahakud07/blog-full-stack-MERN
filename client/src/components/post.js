fdd61eaa597b469e72fbe95999a6b209023b0933
AMahakud07 <abhishekmahakud007@gmail.com>
2023-07-19 20:04:19 +0530

    single page done

--- client/src/components/post.js
+++ client/src/components/post.js
@@ -1,17 +1,22 @@
+import { Link } from 'react-router-dom';
 import '../App.css';
 import {format} from "date-fns";
 
 
-export default function Post({title,summary,cover,content,createdAt,author}){
+export default function Post({_id,title,summary,cover,content,createdAt,author}){
   // console.log(title);
 
 return (
     <div className='post'>
         <div className='image'>
-        <img src={'http://localhost:4000/'+cover} alt=''></img>
+          <Link to={`/post/${_id}`}>
+          <img src={'http://localhost:4000/'+cover} alt=''></img>
+          </Link>
         </div>
-        <div className="texts">
+        <div className="text">
+          <Link to={`/post/${_id}`}>
           <h2>{title}</h2>
+          </Link>
           <p className='info'>
             <a className='author'>{author.username}</a>
             <time>{format(new Date(createdAt), 'MMM d, yyyy HH:mm')}</time>
